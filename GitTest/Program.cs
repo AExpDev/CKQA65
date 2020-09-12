7784f9643b1dabc7d48c5105e5e1a482abf34c2c
Inna <inna.vdovina@gmail.com>
2020-09-12 20:50:20 +0300

    Changed Class Client

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -9,14 +9,14 @@ namespace GitTest
         {
             List<IDrawable> drawableClasses = new List<IDrawable>();
             drawableClasses.Add(new Chelovek());
-            drawableClasses.Add(new ClassAdress() {Id = 0, City="Tatiana" });
+            drawableClasses.Add(new ClassAdress() {Id = "0", City="Tatiana" });
             drawableClasses.Add(new ClassRooms());
             drawableClasses.Add(new Consultant());
             drawableClasses.Add(new Hostel() {id = 5, name = "Alexandr" });
             drawableClasses.Add(new Respect());
             drawableClasses.Add(new Subject());
             drawableClasses.Add(new User());
-            drawableClasses.Add(new Client() { Name = "Inna"});
+            drawableClasses.Add(new Client() { Id = "9", Name = "Inna"});
 
             foreach (var item in drawableClasses) 
             {
