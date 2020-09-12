ca97bcf220a518d1c1c65dae620f6c48311442ff
Tatiana <eta37979@gmail.com>
2020-09-12 21:49:20 +0300

    Change ID

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -9,7 +9,7 @@ namespace GitTest
         {
             List<IDrawable> drawableClasses = new List<IDrawable>();
             drawableClasses.Add(new Chelovek() { Id = 0, Name = "Denis" });
-            drawableClasses.Add(new ClassAdress() {Id = "1", City="Tatiana" });
+            drawableClasses.Add(new ClassAdress() {Id = 2, Name="Tatiana" });
             drawableClasses.Add(new ClassRooms() { Id = "3", Name = "Nadya" });
             drawableClasses.Add(new Consultant() { Id = 4, Name = "Tanya" });
             drawableClasses.Add(new Hostel() {id = 5, name = "Alexandr" });
